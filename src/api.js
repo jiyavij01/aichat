0b9041e8a968ed464273dcc846639d027586a9af
jiyavij01 <jiyavij0212@gmail.com>
2026-05-27 12:44:39 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http:chat-production-700a.up.railway.app/api/chat/response";
-
+export const API_BASE_URL = "chat-production-700a.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
